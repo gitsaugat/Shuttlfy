1696e7607000816e279061f9581c1316f0c128a2
gitsaugat <letnotsay@gmail.com>
2024-10-21 11:49:07 -0400

    Initial Setup

---
+++ app/(tabs)/shuttle.tsx
@@ -0,0 +1,36 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Image, Platform } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function Shuttle() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={360} name="code-slash" style={styles.headerImage} />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Explore</ThemedText>
+      </ThemedView>
+      <ThemedText>
+        Hello , This app includes example code to help you get started.
+      </ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "orange",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
